3d6640ad1b4daf56be0241bd78b08c4fe230490e
Nitin <niting@axioned.com>
2021-10-20 17:03:58 +0530

    js created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
-/* Author: 
+//variable declerations
+var countersList = document.querySelectorAll(".counter-item span");
+var flag = false;
 
-*/
+//value reset to zero and increment
+countersList.forEach(function(val){
+    var n = parseInt(val.innerHTML);
+    val.innerHTML = "0";
+    var steps = 100;
+    var incrimrnt = Math.round(n/steps);
+    var timer = setInterval(function(){
+        if(parseInt(val.innerHTML)<=n)
+             flag && (val.innerHTML = (parseInt(val.innerHTML)+incrimrnt));
+        else{
+            val.innerHTML = n;
+            clearInterval(timer);
+        }
+    },20)
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//scroll event listner
+document.addEventListener("scroll", function(){ flag = true })
 
 
 
